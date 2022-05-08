89a6cf54d669f9c01c97fc5455a1ae54182be315
Daniil Haiduk <danil.gayduk2002@gmail.com>
2022-05-08 15:16:14 +0300

    Done with filtration and sorting products

--- src/views/Product/Product.vue
+++ src/views/Product/Product.vue
@@ -6,11 +6,27 @@
         <router-link v-if="userEmail" :to="{name:'AddProduct'}">
           <button class="btn" style="float: right">Add Product</button>
         </router-link>
+
+        <div style="">
+          <select class="form-control" v-model="sort" style="width: 20%; display: inline-block; margin-right: 1%" >
+            <option value="id" selected>Order</option>
+            <option value="name">Name</option>
+            <option value="description" >Description</option>
+            <option value="price" >Price</option>
+          </select>
+          <input class="form-control" placeholder="Search" style="width: 20%;display: inline-block;margin-right: 1%"  v-model="search" />
+          <input class="form-control" placeholder="Price start" style="width: 10%;display: inline-block;margin-right: 1%"  v-model="priceStart"/>
+          <input class="form-control" placeholder="Price end" style="width: 10%;display: inline-block; margin-right: 1%"  v-model="priceEnd"/>
+
+          <button type="button" class="btn btn-primary" style="display: inline-block; margin-bottom: 3px" @click="productSortAndFilter()">Submit</button>
+        </div>
+
+
       </div>
     </div>
     <div class="row">
 <!--      Display all the products in product box component-->
-      <div v-for="product of products" :key="product.id"
+      <div v-for="product of filteredProducts" :key="product.id"
            class="col-md-6 col-xl-4 col-12 pt-3">
         <ProductBox :product="product"
                     :userEmail = "userEmail"></ProductBox>
@@ -26,8 +42,80 @@ export default {
   name: "Product",
   components: {ProductBox},
   props:["products", "userEmail"],
+  data(){
+    return {
+      sort:null,
+      search:null,
+      priceStart:null,
+      priceEnd:null,
+      thisProducts:null,
+      filteredProducts:null,
+    }
+  },
+  methods:{
+    productSortAndFilter() {
+
+      switch (this.sort){
+        case "id":
+          this.thisProducts.sort(function(a, b){return a.id - b.id});
+          break;
+        case "name":
+          this.thisProducts.sort(function(a, b){return a.name.localeCompare(b.name);});
+          break;
+        case "description":
+          this.thisProducts.sort(function(a, b){return a.description.length-b.description.length});
+          break;
+        case "price":
+          this.thisProducts.sort(function(a, b){return a.price - b.price});
+          break;
+      }
+      this.filteredProducts = this.thisProducts;
+
+      console.log(this.thisProducts);
+      console.log(this.filteredProducts);
+      console.log(this.priceStart);
+      console.log(this.priceEnd);
+
+      /////////
+      if(this.search!=null){
+        let filteredByName = this.thisProducts.filter(x=>x.name.toLowerCase().includes(this.search.toLowerCase()));
+        let filteredByCode = this.thisProducts.filter(x=>x.code.toLowerCase().includes(this.search.toLowerCase()));
+        let result =filteredByCode.concat(filteredByName);
+
+        this.filteredProducts = result.filter(function(item, pos) {
+          return result.indexOf(item) == pos;
+        })
+      }
+
+      console.log(this.thisProducts);
+      console.log(this.filteredProducts);
+      console.log(this.priceStart);
+      console.log(this.priceEnd);
+      /////////
+
+      if(this.priceStart!=null && this.priceStart!=""){
+        this.filteredProducts = this.filteredProducts.filter(x=>x.price>=this.priceStart);
+      }
+
+      console.log(this.thisProducts);
+      console.log(this.filteredProducts);
+      console.log(this.priceStart);
+      console.log(this.priceEnd);
+
+      if(this.priceEnd!=null && this.priceEnd!=""){
+        this.filteredProducts = this.filteredProducts.filter(x=>x.price<=this.priceEnd);
+      }
+
+      console.log(this.thisProducts);
+      console.log(this.filteredProducts);
+      console.log(this.priceStart);
+      console.log(this.priceEnd);
+    },
+  },
   mounted() {
     this.$emit("fetchData");
+    this.thisProducts = this.products;
+    this.productSortAndFilter();
   }
 }
 </script>
